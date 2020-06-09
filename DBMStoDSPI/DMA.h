0286626bc3f0632a4ae8f6d65a467f71ff7790a8
Kaspar Matas <kasparmat@hotmail.com>
2020-06-09 13:30:36 +0100

    Add project files.

---
+++ DBMStoDSPI/DMA.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <cstdint>
+class DMA
+{
+private:
+	int* volatile controlAXIbaseAddress;
+	uint32_t contollerModulePosition;
+
+	int* volatile calculateMemoryMappedAddress(int* volatile controlAXIbaseAddress, uint32_t modulePosition, uint32_t moduleInternalAddress);
+	void writeToModule(int* volatile controlAXIbaseAddress, uint32_t modulePosition, uint32_t moduleInternalAddress, uint32_t writeData);
+	uint32_t readFromModule(int* volatile controlAXIbaseAddress, uint32_t modulePosition, uint32_t moduleInternalAddress);
+
+public:
+	DMA(int* volatile ctrlAXIbaseAddress);
+
+	void setInputControllerParams(int streamID, int DDRburstSize, int recordsPerDDRBurst, int bufferStart, int bufferEnd);
+	uint32_t getInputControllerParams(int streamID);
+	void setInputControllerStreamAddress(int streamID, uintptr_t address);
+	uintptr_t getInputControllerStreamAddress(int streamID);
+	void setInputControllerStreamSize(int streamID, int size);
+	uint32_t getInputControllerStreamSize(int streamID);
+	void startInputController(bool streamActive[16]);
+	bool isInputControllerFinished();
+
+	void setRecordSize(int streamID, int recordSize);
+	void setRecordChunkIDs(int streamID, int interfaceCycle, int chunkID);
+
+	void setOutputControllerParams(int streamID, int DDRburstSize, int recordsPerDDRBurst, int bufferStart, int bufferEnd);
+	uint32_t getOutputControllerParams(int streamID);
+	void setOutputControllerStreamAddress(int streamID, uintptr_t address);
+	uintptr_t getOutputControllerStreamAddress(int streamID);
+	void setOutputControllerStreamSize(int streamID, int size);
+	uint32_t getOutputControllerStreamSize(int streamID);
+	void startOutputController(bool streamActive[16]);
+	bool isOutputControllerFinished();
+
+	void setBufferToInterfaceChunk(int streamID, int clockCycle, int offset, int sourceChunk4, int sourceChunk3, int sourceChunk2, int sourceChunk1);
+	void setBufferToInterfaceSourcePosition(int streamID, int clockCycle, int offset, int sourcePosition4, int sourcePosition3, int sourcePosition2, int sourcePosition1);
+
+	void setAXItoBufferChunk(int streamID, int clockCycle, int offset, int targetChunk4, int targetChunk3, int targetChunk2, int targetChunk1);
+	void setAXItoBufferSourcePosition(int streamID, int clockCycle, int offset, int sourcePosition4, int sourcePosition3, int sourcePosition2, int sourcePosition1);
+
+	void setInterfaceToBufferChunk(int streamID, int clockCycle, int offset, int targetChunk4, int targetChunk3, int targetChunk2, int targetChunk1);
+	void setInterfaceToBufferSourcePosition(int streamID, int clockCycle, int offset, int sourcePosition4, int sourcePosition3, int sourcePosition2, int sourcePosition1);
+
+	void setBufferToAXIChunk(int streamID, int clockCycle, int offset, int sourceChunk4, int sourceChunk3, int sourceChunk2, int sourceChunk1);
+	void setBufferToAXISourcePosition(int streamID, int clockCycle, int offset, int sourcePosition4, int sourcePosition3, int sourcePosition2, int sourcePosition1);
+};
+
---
+++ DBMStoDSPI/main.cpp
@@ -0,0 +1,238 @@
+#include <cstdio>
+#include "DMA.h"
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include "rapidcsv.h"
+
+/*
+Filter: (price < 12000)
+1000 rows
+ Column |         Type          |
+--------+-----------------------+
+ id     | bigint                |
+ make   | character varying(32) |
+ model  | character varying(32) |
+ price  | integer               |
+*/
+
+struct stringAsciiValue
+{
+	std::vector<int> values;
+	stringAsciiValue() :values(32 / 4, 0) {}
+};
+
+void ConvToInt(const std::string& pStr, stringAsciiValue& pVal)
+{
+	for (int i = 0; i < pStr.length(); i++) {
+		pVal.values[i / 4] += int(pStr[i]) << (i % 4) * 8;
+	}
+}
+
+int main()
+{
+	// Figure out some legit way to get this data type information. For all streams
+	std::vector<int> dataTypeSizes;
+	dataTypeSizes.push_back(1);
+	dataTypeSizes.push_back(8);
+	dataTypeSizes.push_back(8);
+	dataTypeSizes.push_back(1);
+	int recordSize = 0;
+	for (auto i = dataTypeSizes.begin(); i != dataTypeSizes.end(); ++i) {
+		recordSize += *i;
+	}
+
+	// The data probably doesn't come in a csv but it'll do for now
+	rapidcsv::Document doc("MOCK_DATA.csv", rapidcsv::LabelParams(-1, -1));
+	int recordCount = doc.GetRowCount();
+
+	// Create contiguous data array
+	std::vector<int> dbData(doc.GetRowCount() * recordSize);
+	int currentLocalDataAddress = 0;
+	for (int rowNumber = 0; rowNumber < recordCount; rowNumber++) {
+		for (int colNumber = 0; colNumber < 4; colNumber++) {
+			switch (colNumber) {
+			case 0:
+			case 3:
+				dbData[currentLocalDataAddress++] = doc.GetCell<int>(colNumber, rowNumber);
+				break;
+			case 1:
+			case 2:
+				stringAsciiValue asciiValue = doc.GetCell<stringAsciiValue>(colNumber, rowNumber, ConvToInt);
+				for (int i = 0; i < asciiValue.values.size(); i++) {
+					dbData[currentLocalDataAddress++] = asciiValue.values[i];
+				}
+				break;
+			}
+		}
+	}
+
+	// Create the controller memory area
+	int* volatile memoryPointer = new int[1048576];
+	for (int i = 0; i < 1048576; i++) {
+		memoryPointer[i] = -1;
+	}
+	DMA dmaEngine(memoryPointer);
+	// Calculate the controller parameter values based on input data and datatypes
+	// Every size metric is 1 integer = 4 bytes = 32 bits
+	const int maxDDRBurstSize = 512;
+	const int maxChunkSize = 16;
+	const int maxDDRSizePerCycle = 4;
+
+	//Input
+	int inputStreamID = 0;
+
+	int chunksPerRecord = (recordSize + maxChunkSize - 1) / maxChunkSize; //ceil
+	std::cout << "chunksPerRecord:" << chunksPerRecord << std::endl;
+
+	int recordsPerMaxBurstSize = maxDDRBurstSize / recordSize;
+	int recordsPerDDRBurst = pow(2, (int)log2(recordsPerMaxBurstSize));
+	std::cout << "recordsPerDDRBurst:" << recordsPerDDRBurst << std::endl;
+
+	int DDRBurstLength = ((recordSize * recordsPerDDRBurst) + maxDDRSizePerCycle - 1) / maxDDRSizePerCycle;
+	std::cout << "DDRBurstLength:" << DDRBurstLength << std::endl;
+
+	int bufferStart = 0;
+	int bufferEnd = 15;
+
+	// Set input controller params
+	dmaEngine.setInputControllerParams(inputStreamID, DDRBurstLength, recordsPerDDRBurst, bufferStart, bufferEnd);
+	dmaEngine.setInputControllerStreamAddress(inputStreamID, reinterpret_cast<uintptr_t>(&dbData[0]));
+	dmaEngine.setInputControllerStreamSize(inputStreamID, recordCount);
+	dmaEngine.setRecordSize(inputStreamID, chunksPerRecord);
+	for (int i = 0; i < chunksPerRecord; i++) {
+		dmaEngine.setRecordChunkIDs(inputStreamID, i, i);
+	}
+
+	// Output
+	int outputStreamID = 1;
+	recordCount = 0;
+	std::vector<int> outputData(doc.GetRowCount() * recordSize);
+
+	chunksPerRecord = (recordSize + maxChunkSize - 1) / maxChunkSize; //ceil
+	std::cout << "chunksPerRecord:" << chunksPerRecord << std::endl;
+
+	recordsPerMaxBurstSize = maxDDRBurstSize / recordSize;
+	recordsPerDDRBurst = pow(2, (int)log2(recordsPerMaxBurstSize));
+	std::cout << "recordsPerDDRBurst:" << recordsPerDDRBurst << std::endl;
+
+	DDRBurstLength = ((recordSize * recordsPerDDRBurst) + maxDDRSizePerCycle - 1) / maxDDRSizePerCycle;
+	std::cout << "DDRBurstLength:" << DDRBurstLength << std::endl;
+
+	bufferStart = 0;
+	bufferEnd = 15;
+
+	// Set input controller params
+	dmaEngine.setOutputControllerParams(outputStreamID, DDRBurstLength, recordsPerDDRBurst, bufferStart, bufferEnd);
+	dmaEngine.setOutputControllerStreamAddress(outputStreamID, reinterpret_cast<uintptr_t>(&outputData[0]));
+	dmaEngine.setOutputControllerStreamSize(outputStreamID, recordCount);
+	dmaEngine.setRecordSize(outputStreamID, chunksPerRecord);
+	for (int i = 0; i < chunksPerRecord; i++) {
+		dmaEngine.setRecordChunkIDs(outputStreamID, i, i);
+	}
+
+
+	// Then burn pointless cycles between AXI and Buffer 4 times.
+
+	dmaEngine.setAXItoBufferChunk(inputStreamID,0,0,0,0,0,0);
+	dmaEngine.setAXItoBufferSourcePosition(inputStreamID, 0, 0, 0, 1, 2, 3);
+
+	dmaEngine.setBufferToInterfaceChunk(inputStreamID);
+	dmaEngine.setBufferToInterfaceSourcePosition(inputStreamID);
+
+	dmaEngine.setInterfaceToBufferChunk(outputStreamID);
+	dmaEngine.setInterfaceToBufferSourcePosition(outputStreamID);
+
+	dmaEngine.setBufferToAXIChunk(outputStreamID);
+	dmaEngine.setBufferToAXISourcePosition(outputStreamID);
+
+
+	/*
+	struct {
+		int data[16];
+		int chunkID;
+	} RecordOnInterface[32];
+	*/
+
+	/*
+	struct {
+		int data[16];
+	} Buffer_block[32];
+	*/
+
+	/*
+	numRecordsPerBurst = 16;
+	streamID = 0;
+	recordSize = 2;
+	*/
+
+	/*
+	int tarbetChunk[DDRburstSize][16];
+	int sourcePosition[DDRburstSize][16];
+	for(int clockCycle=0; clockCycle < DDRburstSize; clockCycle++){
+		for(int dataPos=0; dataPos<16; dataPos++){
+			int targetChunkTemp = targetChunk[clockCycle][dataPos];
+			int sourcePositionTemp = sourcePosition[clockCycle][dataPos];
+			Buffer_block[targetChunkTemp].data[dataPos]=AXI_Burst[clockCycle].data[sourcePositionTemp];
+		}
+	}
+	*/
+
+
+	/*
+	int sourceChunkB2I[32][16];
+	int sourcePositionB2I[32][16];
+	for(int cyclePosition = numRecordsPerBurst*recordID; cyclePosition < numRecordsPerBurst*recordID + recordSize; cyclePosition++){
+		int dataRead[16];
+		for(int dataPos=0; dataPos <16; dataPos++){
+			int sourceChunkTemp = sourceChunkB2I[cyclePosition][dataPos];
+			dataRead[dataPos] = Buffer_block[sourceChunkTemp].data[dataPos];
+		}
+		for(int dataPos = 0; dataPos <16; dataPost++){
+			int sourcePositionTemp = sourcePositionB2I[cyclePosition][dataPos];
+			RecordOnInterface[cyclePosition].data[dataPos]=dataRead[SourcePositionTemp];
+		}
+		RecordOnInterface[cyclePosition].chunkID = chunkIDTranslate[cyclePosition];
+	}
+	All 16 positions always
+
+	*/
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// Print out the contents of memory for debugging
+	std::cout << std::endl << "Memory contents:" << std::endl;
+	for (int i = 0; i < 1048576; i++) {
+		if (memoryPointer[i] != -1) {
+			std::cout << "Address:" << i << std::endl;
+			std::cout << memoryPointer[i] << std::endl;
+		}
+	}
+
+	delete[] memoryPointer;
+	return 0;
+}
