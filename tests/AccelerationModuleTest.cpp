e5ee5d322e4a5e67814ae2fffc821e7f68c96ca7
Kaspar Matas <kasparmat@hotmail.com>
2020-07-13 13:53:59 +0100

    Added test fixture tests
    Added initial test fixture tests where multiple AccelerationModule tests use the same setup.

--- tests/AccelerationModuleTest.cpp
+++ tests/AccelerationModuleTest.cpp
@@ -2,42 +2,54 @@
 
 #include <gtest/gtest.h>
 #include "AccelerationModuleMock.hpp"
+namespace {
+	const int DEFAULT_VALUE = -1;
 
-TEST(AccelerationModuleTest, testCase) {
-	const int defaultValue = -1;
+	class AccelerationModuleTest : public testing::Test {
+	protected:
+		void SetUp() override {
+			memoryPointer = new int[2097152];
+			for (int i = 0; i < 2097152; i++) {
+				memoryPointer[i] = DEFAULT_VALUE;
+			}
+		}
+		void TearDown() override {
+			delete[] memoryPointer;
+		}
+		int* volatile memoryPointer = nullptr;
+	};
 
-	int* volatile memoryPointer = new int[2097152];
-	for (int i = 0; i < 2097152; i++) {
-		memoryPointer[i] = defaultValue;
+	TEST_F(AccelerationModuleTest, WriteToModule) {
+		AccelerationModuleMock mockModule(memoryPointer, 0);
+
+		EXPECT_EQ(DEFAULT_VALUE, memoryPointer[0]);
+		mockModule.writeToModule(0, 0);
+		EXPECT_EQ(0, memoryPointer[0]);
+
+		EXPECT_EQ(DEFAULT_VALUE, memoryPointer[1]);
+		mockModule.writeToModule(1, 10);
+		EXPECT_EQ(10, memoryPointer[1]);
+
+		AccelerationModuleMock secondMockModule(memoryPointer, 1);
+
+		EXPECT_EQ(DEFAULT_VALUE, memoryPointer[1048576]);
+		secondMockModule.writeToModule(0, 1);
+		EXPECT_EQ(1, memoryPointer[1048576]);
+		secondMockModule.writeToModule(0, 11);
+		EXPECT_EQ(11, memoryPointer[1048576]);
 	}
 
-	AccelerationModuleMock mockModule(memoryPointer, 0);
+	TEST_F(AccelerationModuleTest, ReadFromModule) {
+		AccelerationModuleMock mockModule(memoryPointer, 0);
 
-	EXPECT_EQ(defaultValue, memoryPointer[0]);
-	mockModule.writeToModule(0, 0);
-	EXPECT_EQ(0, memoryPointer[0]);
+		EXPECT_EQ(DEFAULT_VALUE, mockModule.readFromModule(2));
+		memoryPointer[2] = 100;
+		EXPECT_EQ(100, mockModule.readFromModule(2));
 
-	EXPECT_EQ(defaultValue, memoryPointer[1]);
-	mockModule.writeToModule(1, 10);
-	EXPECT_EQ(10, memoryPointer[1]);
+		AccelerationModuleMock secondMockModule(memoryPointer, 1);
 
-	EXPECT_EQ(defaultValue, mockModule.readFromModule(2));
-	memoryPointer[2] = 100;
-	EXPECT_EQ(100, mockModule.readFromModule(2));
-
-	AccelerationModuleMock secondMockModule(memoryPointer, 1);
-
-	EXPECT_EQ(defaultValue, memoryPointer[1048576]);
-	secondMockModule.writeToModule(0, 1);
-	EXPECT_EQ(1, memoryPointer[1048576]);
-
-	EXPECT_EQ(defaultValue, memoryPointer[1048577]);
-	secondMockModule.writeToModule(1, 11);
-	EXPECT_EQ(11, memoryPointer[1048577]);
-
-	EXPECT_EQ(defaultValue, secondMockModule.readFromModule(2));
-	memoryPointer[1048578] = 101;
-	EXPECT_EQ(101, secondMockModule.readFromModule(2));
-
-	delete[] memoryPointer;
-}
+		EXPECT_EQ(DEFAULT_VALUE, secondMockModule.readFromModule(2));
+		memoryPointer[1048578] = 101;
+		EXPECT_EQ(101, secondMockModule.readFromModule(2));
+	}
+}  // namespace
