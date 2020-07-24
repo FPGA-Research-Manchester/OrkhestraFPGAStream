94194459df9c6f522a42f14dbc6c474bddf3507a
Kaspar Matas <kasparmat@hotmail.com>
2020-07-24 15:01:21 +0100

    Specified DMASetupTest golden data
    Changed the DMASetup unit test's expected data to be concrete expected values. Additionally, removed commented out print statements from the DMA module.

--- tests/DMASetupTest.cpp
+++ tests/DMASetupTest.cpp
@@ -2,31 +2,50 @@
 #include <gmock/gmock.h>
 #include "MockDMA.hpp"
 #include "DMASetup.hpp"
+#include <cstdio>
 namespace {
 	TEST(DMASetupTest, InputParamsSettings) {
+		int expectedInputStreamID = 0;
+		int expectedDDRBurstLength = 72;
+		int expectedRecordsPerDDRBurst = 16;
+		int expectedBufferStart = 0;
+		int expectedBufferEnd = 15;
+		int expectedStreamSize = 1000;
+		std::vector<int> mockDBData(1, 0);
 		MockDMA mockDMA;
-		EXPECT_CALL(mockDMA, setInputControllerParams(testing::_, testing::_, testing::_, testing::_, testing::_)).Times(1);
-		EXPECT_CALL(mockDMA, setInputControllerStreamAddress(testing::_, testing::_)).Times(1);
-		EXPECT_CALL(mockDMA, setInputControllerStreamSize(testing::_, testing::_)).Times(1);
+		EXPECT_CALL(mockDMA, setInputControllerParams(expectedInputStreamID, expectedDDRBurstLength, expectedRecordsPerDDRBurst, expectedBufferStart, expectedBufferEnd)).Times(1);
+		EXPECT_CALL(mockDMA, setInputControllerStreamAddress(expectedInputStreamID, reinterpret_cast<uintptr_t>(&mockDBData[0]))).Times(1);
+		EXPECT_CALL(mockDMA, setInputControllerStreamSize(expectedInputStreamID, expectedStreamSize)).Times(1);
 		DMASetup DMAConfigurer;
-		std::vector<int> mockDBData(10,0);
-		DMAConfigurer.SetupDMAModule(1, mockDBData, 1, mockDMA);
+		
+		DMAConfigurer.SetupDMAModule(expectedStreamSize, mockDBData, 18, mockDMA);
 	}
 	TEST(DMASetupTest, OutputParamsSettings) {
+		int expectedOutputStreamID = 1;
+		int expectedDDRBurstLength = 72;
+		int expectedRecordsPerDDRBurst = 16;
+		int expectedBufferStart = 0;
+		int expectedBufferEnd = 15;
+		std::vector<int> mockDBData(1, 0);
 		MockDMA mockDMA;
-		EXPECT_CALL(mockDMA, setOutputControllerParams(testing::_, testing::_, testing::_, testing::_, testing::_)).Times(1);
-		EXPECT_CALL(mockDMA, setOutputControllerStreamAddress(testing::_, testing::_)).Times(1);
-		EXPECT_CALL(mockDMA, setOutputControllerStreamSize(testing::_, testing::_)).Times(1);
+		EXPECT_CALL(mockDMA, setOutputControllerParams(expectedOutputStreamID, expectedDDRBurstLength, expectedRecordsPerDDRBurst, expectedBufferStart, expectedBufferEnd)).Times(1);
+		EXPECT_CALL(mockDMA, setOutputControllerStreamAddress(expectedOutputStreamID, reinterpret_cast<uintptr_t>(&mockDBData[0]))).Times(1);
+		EXPECT_CALL(mockDMA, setOutputControllerStreamSize(expectedOutputStreamID, 0)).Times(1);
 		DMASetup DMAConfigurer;
-		std::vector<int> mockDBData(10, 0);
-		DMAConfigurer.SetupDMAModule(1, mockDBData, 1, mockDMA);
+		DMAConfigurer.SetupDMAModule(1000, mockDBData, 18, mockDMA);
 	}
 	TEST(DMASetupTest, RecordSettings) {
+		int expectedOutputStreamID = 1;
+		int expectedInputStreamID = 0;
+		std::vector<int> mockDBData(1, 0);
 		MockDMA mockDMA;
-		EXPECT_CALL(mockDMA, setRecordSize(testing::_, testing::_)).Times(2);
-		//EXPECT_CALL(mockDMA, setRecordChunkIDs(testing::_, testing::_, testing::_));
+		EXPECT_CALL(mockDMA, setRecordSize(expectedOutputStreamID, 2)).Times(1);
+		EXPECT_CALL(mockDMA, setRecordSize(expectedInputStreamID, 2)).Times(1);
+		EXPECT_CALL(mockDMA, setRecordChunkIDs(expectedOutputStreamID, 0, 0)).Times(1);
+		EXPECT_CALL(mockDMA, setRecordChunkIDs(expectedOutputStreamID, 1, 1)).Times(1);
+		EXPECT_CALL(mockDMA, setRecordChunkIDs(expectedInputStreamID, 0, 0)).Times(1);
+		EXPECT_CALL(mockDMA, setRecordChunkIDs(expectedInputStreamID, 1, 1)).Times(1);
 		DMASetup DMAConfigurer;
-		std::vector<int> mockDBData(10, 0);
-		DMAConfigurer.SetupDMAModule(1, mockDBData, 1, mockDMA);
+		DMAConfigurer.SetupDMAModule(1000, mockDBData, 18, mockDMA);
 	}
 }
