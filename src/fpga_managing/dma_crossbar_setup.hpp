e3c1e1fee50e163c22ca2214956a8eb0e9d35b19
Kaspar Matas <kasparmat@hotmail.com>
2020-11-11 14:25:33 +0000

    Fixed the failing DMA crossbar setup tests
    Changed the DMA crossbar setup algorithm to pass the tests

--- src/fpga_managing/dma_crossbar_setup.hpp
+++ src/fpga_managing/dma_crossbar_setup.hpp
@@ -14,32 +14,38 @@ class DMACrossbarSetup {
       std::queue<int>& source_chunks, std::queue<int>& target_positions,
       const int& any_chunk, const int& any_position,
       const int& last_chunk_leftover_size, const int& steps_per_cycle,
-      const int& cycle_count, const int& chunks_per_record);
+      const int& cycle_count, const int& chunks_per_record,
+      const float& cycle_step_chunk_increase);
   static void CalculateBufferToInterfaceSetupConfig(
       std::queue<int>& source_chunks, std::queue<int>& target_positions,
       const int& any_chunk, const int& any_position,
       const int& last_chunk_leftover_size, const int& steps_per_cycle,
-      const int& cycle_count, const int& chunks_per_record);
+      const int& cycle_count, const int& chunks_per_record,
+      const float& cycle_step_chunk_increase);
 
   static void InitialDataSetupFromInterfaceToBuffer(
       const int& chunks_per_record, const int& current_cycle_step_initial_chunk,
-      const int& current_offset_point, std::queue<int>& source_chunks,
-      const int& current_position_shift, std::queue<int>& target_positions);
+      const int& current_offset_point, const int& current_position_shift,
+      std::queue<int>& source_chunks, std::queue<int>& target_positions);
   static void FinalDataSetupFromInterfaceToBuffer(
-      const int& current_offset_point, const int& last_chunk_leftover_size,
-      std::queue<int>& source_chunks, const int& any_chunk,
-      std::queue<int>& target_positions, const int& any_position,
+      const int& used_leftover_chunks_count,
       const int& current_cycle_step_initial_chunk,
-      const int& chunks_per_record);
+      const int& last_chunk_leftover_size,
+      const int& chunks_per_record, std::queue<int>& source_chunks,
+      const int& any_chunk, std::queue<int>& target_positions,
+      const int& any_position);
   
   static void InitialDataSetupFromBufferToInterface(
       const int& chunks_per_record, const int& current_cycle_step_initial_chunk,
       const int& current_offset_point, std::queue<int>& source_chunks,
       std::queue<int>& target_positions);
   static void FinalDataSetupFromBufferToInterface(
-      const int& current_offset_point, const int& last_chunk_leftover_size,
+      const int& used_leftover_chunks_count,
+      const int& current_cycle_step_initial_chunk,
+      const int& current_offset_point,
+      const int& last_chunk_leftover_size,
+      const int& chunks_per_record,
       std::queue<int>& source_chunks, const int& any_chunk,
-      const int& current_cycle_step_initial_chunk, const int& chunks_per_record,
       std::queue<int>& target_positions, const int& any_position);
 
   static void InitialChunkSetupFromBufferToInterface(
@@ -48,13 +54,13 @@ class DMACrossbarSetup {
   static void InitialPositionSetupFromBufferToInterface(
       const int& current_offset_point, std::queue<int>& target_positions);
   static void FinalPositionSetupFromBufferToInterface(
-      const int& last_chunk_leftover_size, std::queue<int>& target_positions,
-      const int& any_position, const int& current_offset_point);
+      const int& last_chunk_leftover_size, const int& current_offset_point,
+      std::queue<int>& target_positions, const int& any_position);
   static void FinalChunkSetupFromBufferToInterface(
-      const int& current_offset_point, const int& last_chunk_leftover_size,
-      std::queue<int>& source_chunks, const int& any_chunk,
-      const int& current_cycle_step_initial_chunk,
-      const int& chunks_per_record);
+      const int& used_leftover_chunks_count,
+      const int& last_chunk_leftover_size,
+      const int& current_cycle_step_initial_chunk, const int& chunks_per_record,
+      std::queue<int>& source_chunks, const int& any_chunk);
 
   static void InitialChunkSetupFromInterfaceToBuffer(
       const int& current_offset_point, std::queue<int>& source_chunks,
