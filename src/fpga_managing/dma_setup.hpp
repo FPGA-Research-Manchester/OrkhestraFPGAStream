e308fcf5d751c0bb6dd8f65d49d98f1a7a0e5cc7
Kaspar Matas <kasparmat@hotmail.com>
2020-12-02 17:17:41 +0000

    Fixed passthrough stream to use the correct buffers
    Changed DMA setup class to correctly assign buffers if streams have odd IDs.

--- src/fpga_managing/dma_setup.hpp
+++ src/fpga_managing/dma_setup.hpp
@@ -15,10 +15,10 @@ class DMASetup {
  private:
   static void AddNewStreamDMASetupData(
       DMASetupData &input_stream_setup_data,
-      StreamDataParameters &stream_init_data, const int &max_chunk_size,
+      const StreamDataParameters &stream_init_data, const int &max_chunk_size,
       const int &max_ddr_burst_size, const int &max_ddr_size_per_cycle,
       const int &any_chunk, const int &any_position, int buffer_size,
-      std::vector<DMASetupData> &setup_data_for_dma);
+      std::vector<DMASetupData> &setup_data_for_dma, int current_stream_count);
   static void WriteSetupDataToDMAModule(
       std::vector<DMASetupData>& setup_data_for_dma, DMAInterface& dma_engine);
   static void SetUpDMAIOStreams(DMASetupData& stream_setup_data,
