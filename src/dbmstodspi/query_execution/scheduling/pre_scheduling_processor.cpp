f16ac42f69d9e5bd01886b8e220d78cf318099fd
Kaspar Matas <kasparmat@hotmail.com>
2021-11-05 13:28:48 +0000

    Add missing method stubs for scheduling preprocessing
    Added method stubs for finding statisfying bitstream locations before starting scheduling.
    
    Issue #102

--- src/dbmstodspi/query_execution/scheduling/pre_scheduling_processor.cpp
+++ src/dbmstodspi/query_execution/scheduling/pre_scheduling_processor.cpp
@@ -22,17 +22,53 @@ auto PreSchedulingProcessor::GetMinimumCapacityValuesFromHWLibrary(
     const std::map<QueryOperationType, OperationPRModules>& hw_library)
     -> std::map<QueryOperationType, std::vector<int>> {
   std::map<QueryOperationType, std::vector<int>> min_capacity_map;
-  for ( const auto &[operation, operation_bitstreams_data]: hw_library ) {
-    for ( const auto &[bitstream_name, bitstream_parameters]: operation_bitstreams_data.bitstream_map) {
-      const auto &[operation_iterator, inserted_flag] = min_capacity_map.try_emplace(operation, bitstream_parameters.capacity);
+  for (const auto& [operation, operation_bitstreams_data] : hw_library) {
+    for (const auto& [bitstream_name, bitstream_parameters] :
+         operation_bitstreams_data.bitstream_map) {
+      const auto& [operation_iterator, inserted_flag] =
+          min_capacity_map.try_emplace(operation,
+                                       bitstream_parameters.capacity);
       if (!inserted_flag) {
-        for (int capacity_param_index = 0; capacity_param_index < bitstream_parameters.capacity.size(); capacity_param_index++){
-          if (operation_iterator->second.at(capacity_param_index) > bitstream_parameters.capacity.at(capacity_param_index)) {
-            operation_iterator->second[capacity_param_index] = bitstream_parameters.capacity.at(capacity_param_index);
+        for (int capacity_param_index = 0;
+             capacity_param_index < bitstream_parameters.capacity.size();
+             capacity_param_index++) {
+          if (operation_iterator->second.at(capacity_param_index) >
+              bitstream_parameters.capacity.at(capacity_param_index)) {
+            operation_iterator->second[capacity_param_index] =
+                bitstream_parameters.capacity.at(capacity_param_index);
           }
         }
       }
     }
   }
   return min_capacity_map;
+}
+
+auto PreSchedulingProcessor::GetNewAvailableNodesAfterSchedulingGivenNode(
+    std::string node_name, const std::vector<std::string>& past_nodes,
+    const std::map<std::string, SchedulingQueryNode>& graph)
+    -> std::vector<std::string> {
+  return std::vector<std::string>();
+}
+
+auto PreSchedulingProcessor::GetMinRequirementsForFullyExecutingNode(
+    std::string node_name,
+    const std::map<std::string, SchedulingQueryNode>& graph,
+    const std::map<QueryOperationType, OperationPRModules>& hw_library,
+    const std::vector<TableMetadata> data_tables) -> std::vector<int> {
+  return std::vector<int>();
+}
+
+auto PreSchedulingProcessor::FindAdequateBitstreams(
+    const std::vector<int> min_requirements, QueryOperationType operation,
+    const std::map<QueryOperationType, OperationPRModules>& hw_library)
+    -> std::vector<std::string> {
+  return std::vector<std::string>();
+}
+
+auto PreSchedulingProcessor::GetFittingBitstreamLocations(
+    const std::vector<std::string>& fitting_bitstreams,
+    const std::vector<std::vector<std::string>>& start_locations)
+    -> std::vector<std::vector<std::string>> {
+  return std::vector<std::vector<std::string>>();
 }
--- src/dbmstodspi/query_execution/scheduling/pre_scheduling_processor.hpp
+++ src/dbmstodspi/query_execution/scheduling/pre_scheduling_processor.hpp
@@ -21,9 +21,13 @@ limitations under the License.
 
 #include "operation_types.hpp"
 #include "pr_module_data.hpp"
+#include "scheduling_query_node.hpp"
+#include "table_data.hpp"
 
 using orkhestrafs::core_interfaces::hw_library::OperationPRModules;
 using orkhestrafs::core_interfaces::operation_types::QueryOperationType;
+using orkhestrafs::dbmstodspi::SchedulingQueryNode;
+using orkhestrafs::core_interfaces::table_data::TableMetadata;
 
 namespace orkhestrafs::dbmstodspi {
 
@@ -40,6 +44,37 @@ class PreSchedulingProcessor {
   static auto GetMinimumCapacityValuesFromHWLibrary(
       const std::map<QueryOperationType, OperationPRModules>& hw_library)
       -> std::map<QueryOperationType, std::vector<int>>;
+
+  //def get_new_available_nodes(scheduled_node, past_nodes, all_nodes):
+  static auto GetNewAvailableNodesAfterSchedulingGivenNode(
+      std::string node_name, const std::vector<std::string>& past_nodes,
+      const std::map<std::string, SchedulingQueryNode>& graph)
+      -> std::vector<std::string>;
+
+  // def get_min_requirements(current_node_name, graph, hw_library, data_tables)
+  static auto GetMinRequirementsForFullyExecutingNode(
+      std::string node_name,
+      const std::map<std::string, SchedulingQueryNode>& graph,
+      const std::map<QueryOperationType, OperationPRModules>& hw_library,
+      const std::vector<TableMetadata> data_tables) -> std::vector<int>;
+
+  // def find_adequate_bitstreams(min_requirements, operation, hw_library)
+  static auto FindAdequateBitstreams(
+      const std::vector<int> min_requirements, QueryOperationType operation,
+      const std::map<QueryOperationType, OperationPRModules>& hw_library)
+      -> std::vector<std::string>;
+
+  // def get_fitting_bitstream_locations_based_on_list(list_of_fitting_bitstreams, start_locations)
+  static auto GetFittingBitstreamLocations(
+      const std::vector<std::string>& fitting_bitstreams,
+      const std::vector<std::vector<std::string>>& start_locations)
+      -> std::vector<std::vector<std::string>>;
+
+  // def add_new_table_to_next_nodes_in_place(all_nodes, node, table_names)
+
+  // def get_worst_case_fully_processed_tables(input_tables, current_node_decorators, data_tables, min_capacity)
+
+  // def add_satisfying_bitstream_locations_to_graph(available_nodes, graph, hw_library, data_tables)
 };
 
 }  // namespace orkhestrafs::dbmstodspi
