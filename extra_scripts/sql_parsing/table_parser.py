d84de0664a3880e6f20c2eb6148eae09339715c9
Kaspar Matas <kaspar.matas@manchester.ac.uk>
2022-06-16 11:21:27 +0100

    Demonstrate reading table information from PostgreSQL
    Changed the table_parser.py script in extra scripts to read the information directly from PostgreSQL and print out the parameters necessary for SQL parsing API.

--- extra_scripts/sql_parsing/table_parser.py
+++ extra_scripts/sql_parsing/table_parser.py
@@ -13,18 +13,46 @@
 # limitations under the License.
 
 import sys
+from os.path import exists
+import os
+import subprocess
 
+def GetDataType(datatype_string):
+    size_params = datatype_string.split("(")
+    datatype_name = size_params[0]
+    data_size = 1
+    if (len(size_params) != 1):
+        data_size = size_params[1][:-1]
+    datatype_dict = {"bigint":"integer", "integer":"integer", "numeric":"decimal", "character":"varchar", "date":"date", "character varying":"varchar"}
+    return (datatype_dict[datatype_name], data_size)
 
 def main(argv):
-    argv.append("lineitem_table.txt")
-    with open(argv[0]) as f:
+    command = f"echo '\d {argv[0]}' | psql tpch_001"
+    output_filename = f"{argv[0]}.txt"
+    os.system(f"{command} > {output_filename}")
+
+    #csv_filename = argv[0].split('.')[0] + ".csv"
+    csv_filename = argv[0] + ".csv"
+    if (not exists(csv_filename)):
+        pass
+        #raise RuntimeError(f"{csv_filename} doesn't exist!")
+
+    with open(output_filename) as f:
         lines = f.readlines()[3:]
 
+    print("columns =")
     for line in lines:
         values = line.split("|")[:2]
-        for value in values:
-            print(value.strip())
+        if (len(values)>1):
+            data_type,data_size = GetDataType(values[1].strip())
+            print(f"{data_type}; {data_size}; {values[0].strip().upper()}")
 
+    column_count_command = f"echo 'SELECT count(*) FROM {argv[0]}' | psql tpch_001"
+    count_output = subprocess.check_output(column_count_command, shell=True, text=True)
+    count_lines = count_output.split("\n")
+    row_count = count_lines[2].strip()
+
+    print(f"RegisterTable({csv_filename}, columns, {row_count})")
 
 if __name__ == '__main__':
     main(sys.argv[1:])
